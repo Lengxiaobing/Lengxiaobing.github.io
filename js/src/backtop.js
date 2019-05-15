2e3da25b58cc61eeb3748c5255cb5009da88b355
root <tn787681084@163.com>
2019-05-15 17:06:25 +0800

    添加返回顶部浮动按钮

--- js/src/backtop.js
+++ js/src/backtop.js
@@ -1,18 +1,34 @@
+//圆形返回顶部
+// $("#back_top").hide();
+//
+// $(document).ready(function () {
+//     $(window).scroll(function () {
+//         if ($(this).scrollTop() > 100) {
+//             $('#back_top').fadeIn();
+//         } else {
+//             $('#back_top').fadeOut();
+//         }
+//     });
+//     $('#back_top a').click(function () {
+//         $('body,html').animate({
+//             scrollTop: 0
+//         }, 800);
+//         return false;
+//     });
+// });
 
-$("#back_top").hide();
 
-$(document).ready(function () {
-    $(window).scroll(function () {
-        if ($(this).scrollTop() > 100) {
-            $('#back_top').fadeIn();
-        } else {
-            $('#back_top').fadeOut();
-        }
+//钢铁侠返回顶部
+$(window).scroll(function() {
+    $(window).scrollTop() > $(window).height()*0.5 ? $("#rocket").addClass("show") : $("#rocket").removeClass("show");
+});
+
+$("#rocket").click(function() {
+    $("#rocket").addClass("launch");
+    $("html, body").animate({
+        scrollTop: 0
+    }, 1000, function() {
+        $("#rocket").removeClass("show launch");
     });
-    $('#back_top a').click(function () {
-        $('body,html').animate({
-            scrollTop: 0
-        }, 800);
-        return false;
-    });
+    return false;
 });
